427faed28bfd1a555e17660b78582eda6077457e
taeseok <tsk941229@gmail.com>
2023-03-22 17:49:42 +0900

    index page

---
+++ src/main/java/com/practice/practice1/IndexController.java
@@ -0,0 +1,14 @@
+package com.practice.practice1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/")
+	public String mainPage() {
+		return "/main/index";
+	}
+
+}
