45a9770ba59dc7f11421fd7f96e395e916bc42e3
taeseok <tsk941229@gmail.com>
2023-03-31 11:48:19 +0900

    board crud is completed

--- src/main/java/com/practice/practice1/board/controller/BoardController.java
+++ src/main/java/com/practice/practice1/board/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -98,14 +99,13 @@ public class BoardController {
 		return "redirect:/board/boardList";
 	}
 	
-	@GetMapping("/delete/{id}")
+	@DeleteMapping("/delete/{id}")
 	public String deleteBoard(@PathVariable("id") Long Id) {
 		
 		boardService.delete(boardService.findById(Id).get());
 		
 		return "redirect:/board/boardList";
 	}
-	
 	
 	
 }
