279f4a1e123a7bc783b971996cace73bfb576172
taeseok <tsk941229@gmail.com>
2023-04-14 12:01:31 +0900

    ready to start springsecurity and jwt

---
+++ src/main/java/com/practice/practice1/springsecurityjwt/config/security/WebSecurityConfig.java
@@ -0,0 +1,11 @@
+package com.practice.practice1.springsecurityjwt.config.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfiguration {
+
+	
+	
+}
